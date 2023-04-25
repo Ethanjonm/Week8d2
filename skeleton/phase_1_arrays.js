ea52c01275bb71d4dc78133341885dd285b3af6b
herilynn <4henrylin17@gmail.com>
2023-04-25 14:07:38 -0400

    Finish Phase 1 unique

--- skeleton/phase_1_arrays.js
+++ skeleton/phase_1_arrays.js
@@ -1,5 +1,16 @@
 function uniq(array) {
-    let newArr = []
+    let newArr = [];
 
+    for (let i = 0; i < array.length; i++) {
+        if (!newArr.includes(array[i])) {
+            newArr.push(array[i]) }
+    //         continue }
+    //         else {
+    //         newArr.push(array[i])
+    // }
+    }
+    return newArr;
     
 }
+
+console.log(uniq([ 1, 2, 2, 3, 3, 3 ]))
